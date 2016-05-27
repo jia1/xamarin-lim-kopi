03493488fdf83e8b5c0c2c31cfdbb9fb21ee917c
Jia Yee <jiayeerawr@gmail.com>
2016-05-28 01:07:19 +0800

    Customise basic UI
    Good to have the final order in a single line below

--- xamarin-lim-kopi/Resources/Resource.Designer.cs
+++ xamarin-lim-kopi/Resources/Resource.Designer.cs
@@ -60,8 +60,71 @@ namespace xamarin_lim_kopi
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050015
+			public const int button1 = 2131034133;
+			
+			// aapt resource value: 0x7f050014
+			public const int editText1 = 2131034132;
+			
+			// aapt resource value: 0x7f050002
+			public const int radioButton1 = 2131034114;
+			
+			// aapt resource value: 0x7f05000d
+			public const int radioButton10 = 2131034125;
+			
+			// aapt resource value: 0x7f05000e
+			public const int radioButton11 = 2131034126;
+			
+			// aapt resource value: 0x7f050010
+			public const int radioButton12 = 2131034128;
+			
+			// aapt resource value: 0x7f050011
+			public const int radioButton13 = 2131034129;
+			
+			// aapt resource value: 0x7f050012
+			public const int radioButton14 = 2131034130;
+			
+			// aapt resource value: 0x7f050003
+			public const int radioButton2 = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int radioButton3 = 2131034116;
+			
+			// aapt resource value: 0x7f050006
+			public const int radioButton4 = 2131034118;
+			
+			// aapt resource value: 0x7f050007
+			public const int radioButton5 = 2131034119;
+			
+			// aapt resource value: 0x7f050008
+			public const int radioButton6 = 2131034120;
+			
+			// aapt resource value: 0x7f050009
+			public const int radioButton7 = 2131034121;
+			
+			// aapt resource value: 0x7f05000b
+			public const int radioButton8 = 2131034123;
+			
+			// aapt resource value: 0x7f05000c
+			public const int radioButton9 = 2131034124;
+			
+			// aapt resource value: 0x7f050001
+			public const int radioGroup1 = 2131034113;
+			
+			// aapt resource value: 0x7f050005
+			public const int radioGroup2 = 2131034117;
+			
+			// aapt resource value: 0x7f05000a
+			public const int radioGroup3 = 2131034122;
+			
+			// aapt resource value: 0x7f05000f
+			public const int radioGroup4 = 2131034127;
+			
 			// aapt resource value: 0x7f050000
-			public const int MyButton = 2131034112;
+			public const int toggleButton1 = 2131034112;
+			
+			// aapt resource value: 0x7f050013
+			public const int toggleButton2 = 2131034131;
 			
 			static Id()
 			{
@@ -92,11 +155,59 @@ namespace xamarin_lim_kopi
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
+			// aapt resource value: 0x7f040006
+			public const int Alia = 2130968582;
 			
 			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
+			public const int ApplicationName = 2130968576;
+			
+			// aapt resource value: 0x7f040003
+			public const int Default = 2130968579;
+			
+			// aapt resource value: 0x7f04000a
+			public const int GarDai = 2130968586;
+			
+			// aapt resource value: 0x7f04000c
+			public const int Gau = 2130968588;
+			
+			// aapt resource value: 0x7f040001
+			public const int Kopi = 2130968577;
+			
+			// aapt resource value: 0x7f040009
+			public const int Kosong = 2130968585;
+			
+			// aapt resource value: 0x7f040007
+			public const int Lemon = 2130968583;
+			
+			// aapt resource value: 0x7f04000e
+			public const int MaiPeng = 2130968590;
+			
+			// aapt resource value: 0x7f040008
+			public const int Masala = 2130968584;
+			
+			// aapt resource value: 0x7f040004
+			public const int O = 2130968580;
+			
+			// aapt resource value: 0x7f040011
+			public const int Order = 2130968593;
+			
+			// aapt resource value: 0x7f04000f
+			public const int Peng = 2130968591;
+			
+			// aapt resource value: 0x7f04000d
+			public const int Po = 2130968589;
+			
+			// aapt resource value: 0x7f040005
+			public const int See = 2130968581;
+			
+			// aapt resource value: 0x7f040010
+			public const int Servings = 2130968592;
+			
+			// aapt resource value: 0x7f04000b
+			public const int SiuDai = 2130968587;
+			
+			// aapt resource value: 0x7f040002
+			public const int Teh = 2130968578;
 			
 			static String()
 			{
