8acfecdf420596fb5e5301db858c22984bc675e4
Jia Yee <jiayeerawr@gmail.com>
2016-05-28 19:16:14 +0800

    Formulate the order by checked change on input options

--- xamarin-lim-kopi/MainActivity.cs
+++ xamarin-lim-kopi/MainActivity.cs
@@ -5,6 +5,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Java.Lang;
 
 namespace xamarin_lim_kopi
 {
@@ -28,28 +29,74 @@ namespace xamarin_lim_kopi
             EditText servingsEditText = FindViewById<EditText>(Resource.Id.editServings);
             Button orderButton = FindViewById<Button>(Resource.Id.buttonOrder);
 
+            int milkId = milkRadioGroup.CheckedRadioButtonId;
+            int randomId = randomRadioGroup.CheckedRadioButtonId;
+            int sugarId = sugarRadioGroup.CheckedRadioButtonId;
+            int densityId = densityRadioGroup.CheckedRadioButtonId;
+            int servings = 0;
+
+            string simiDrink = " " + GetString(Resource.String.Kopi);
+            string simiMilk = " " + FindViewById<RadioButton>(milkId).Text;
+            string simiRandom = " " + FindViewById<RadioButton>(randomId).Text;
+            string simiSugar = " " + FindViewById<RadioButton>(sugarId).Text;
+            string thickOrThin = " " + FindViewById<RadioButton>(densityId).Text; ;
+            string pengOrNot = " " + GetString(Resource.String.MaiPeng);
+
+            string[] orderDrink = new string[] {
+                " ",
+                servings.ToString(),
+                simiDrink,
+                simiMilk,
+                simiRandom,
+                simiSugar,
+                pengOrNot
+            };
+
+            orderButton.Text = Core.OrderUpdater.generateOrder(-1, string.Empty, ref orderDrink);
+
+            Int32.TryParse(servingsEditText.Text, out servings);
+
+            // servingsEditText
+
             kopiOrTeh.Click += (object sender, EventArgs e) =>
             {
-                string simiDrink = (kopiOrTeh.Checked) ? 
+                simiDrink = " " + ((kopiOrTeh.Checked) ? 
                     GetString(Resource.String.Teh) 
-                    : GetString(Resource.String.Kopi);
+                    : GetString(Resource.String.Kopi));
+                orderButton.Text = Core.OrderUpdater.generateOrder(2, simiDrink, ref orderDrink);
+            };
+
+            milkRadioGroup.CheckedChange += delegate
+            {
+                simiMilk = " " + FindViewById<RadioButton>(milkId).Text;
+                orderButton.Text = Core.OrderUpdater.generateOrder(3, simiMilk, ref orderDrink);
+            };
+
+            randomRadioGroup.CheckedChange += delegate
+            {
+                simiRandom = " " + FindViewById<RadioButton>(randomId).Text;
+                orderButton.Text = Core.OrderUpdater.generateOrder(4, simiRandom, ref orderDrink);
+            };
+
+            sugarRadioGroup.CheckedChange += delegate
+            {
+                simiSugar = " " + FindViewById<RadioButton>(sugarId).Text;
+                orderButton.Text = Core.OrderUpdater.generateOrder(5, simiSugar, ref orderDrink);
+            };
+
+            densityRadioGroup.CheckedChange += delegate
+            {
+                thickOrThin = " " + FindViewById<RadioButton>(densityId).Text;
+                orderButton.Text = Core.OrderUpdater.generateOrder(6, thickOrThin, ref orderDrink);
             };
 
             pengBo.Click += (object sender, EventArgs e) =>
             {
-                string pengOrNot = (pengBo.Checked) ?
+                pengOrNot = (pengBo.Checked) ?
                     GetString(Resource.String.Peng)
-                    : '(' + GetString(Resource.String.MaiPeng) + ')';
+                    : " (" + GetString(Resource.String.MaiPeng) + ")";
+                orderButton.Text = Core.OrderUpdater.generateOrder(7, pengOrNot, ref orderDrink);
             };
-
-            int servings = 0;
-            if (Int32.TryParse(servingsEditText.Text, out servings))
-            {
-
-            }
-            else {
-                // servingsEditText.Hint = 
-            }
 
             // orderButton.Click += 
         }
---
+++ xamarin-lim-kopi/OrderUpdater.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using System;
+
+namespace Core
+{
+    public static class OrderUpdater
+    {
+        public static string generateOrder(int index, string newOrderPart, ref string[] array)
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i == index)
+                    array[i] = newOrderPart;
+                builder.Append(array[i]);
+            }
+            return builder.ToString();
+        }
+    }
+}
